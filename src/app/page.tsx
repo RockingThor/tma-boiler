1fad39dc8481046fad12a8e67d0b39c15efd64ca
Rohit Nandi <rohitnandibmp@gmail.com>
2024-05-20 22:43:54 +0530

    some silly mistakes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { Me } from "@/components/me";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
--- src/components/takeVote.tsx
+++ src/components/takeVote.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { taskState } from "@/recoil/atom";
 import React from "react";
 import { useRecoilValue } from "recoil";
