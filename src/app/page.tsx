fd7898a5dfc7d751b96581d0b4410a8d713acaaa
Rohit Nandi <rohitnandibmp@gmail.com>
2024-05-23 14:22:41 +0530

    some style change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -53,14 +53,14 @@ export default function Home() {
         setLoading(false);
     }
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between">
+        <main className="flex min-h-screen flex-col items-center justify-center">
             <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
                 <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
                     <code className="font-mono font-bold">OpenPolls🚀</code>
                 </p>
             </div>
 
-            <div className="">
+            <div className="mt-4">
                 {showStart && <Button onClick={getNExtTask}>Start Task</Button>}
                 {loading && (
                     <>
--- src/components/takeVote.tsx
+++ src/components/takeVote.tsx
@@ -9,84 +9,92 @@ import { Task } from "@/lib/type";
 import NoTask from "./noTask";
 
 const TakeVote = () => {
-  const taskData = useRecoilValue(taskState);
-  const [task, setTask] = useRecoilState(taskState);
-  const [loading, setLoading] = useState(false);
-  const [noTask, setNoTask] = useRecoilState(noTaskState);
-  const [balance, setBalance] = useRecoilState(balanceState);
+    const taskData = useRecoilValue(taskState);
+    const [task, setTask] = useRecoilState(taskState);
+    const [loading, setLoading] = useState(false);
+    const [noTask, setNoTask] = useRecoilState(noTaskState);
+    const [balance, setBalance] = useRecoilState(balanceState);
 
-  async function handleImageClick(optionId: number) {
-    setLoading(true);
-    const response = await axios.post(
-      `${BACKEND_URL}/submission`,
-      {
-        taskId: taskData?.id.toString(),
-        selection: optionId.toString(),
-      },
-      {
-        headers: {
-          authorization: localStorage.getItem("token"),
-        },
-      }
+    async function handleImageClick(optionId: number) {
+        setLoading(true);
+        const response = await axios.post(
+            `${BACKEND_URL}/submission`,
+            {
+                taskId: taskData?.id.toString(),
+                selection: optionId.toString(),
+            },
+            {
+                headers: {
+                    authorization: localStorage.getItem("token"),
+                },
+            }
+        );
+        const curBalance = await axios.get(`${BACKEND_URL}/balance`, {
+            headers: {
+                authorization: localStorage.getItem("token"),
+            },
+        });
+        if (curBalance) {
+            console.log(curBalance.data);
+            setBalance(Number(curBalance.data.balance));
+        }
+        if (response) {
+            const nextTask = await axios.get(`${BACKEND_URL}/nextTask`, {
+                headers: {
+                    authorization: localStorage.getItem("token"),
+                },
+            });
+            if (!nextTask.data?.tasks) {
+                setNoTask(true);
+            } else {
+                let data: Task = {
+                    id: nextTask.data.tasks.id,
+                    title: nextTask.data.tasks.title,
+                    options: nextTask.data.tasks.options,
+                };
+                setTask(data);
+            }
+        }
+        setLoading(false);
+    }
+    return (
+        <>
+            <div className="mt-4 items-center">
+                {!loading && !noTask && (
+                    <div className="">
+                        <div className="font-mono text-lg font-bold p-2">
+                            {taskData?.title}
+                        </div>
+                        <div className="p-1">
+                            {taskData?.options.map((option) => (
+                                <div
+                                    className="p-1"
+                                    key={option.id}
+                                >
+                                    <Image
+                                        src={option.image_url}
+                                        alt={option.id.toString()}
+                                        height={400}
+                                        width={400}
+                                        onClick={() => {
+                                            handleImageClick(option.id);
+                                        }}
+                                        className="cursor-pointer border-black rounded m-2"
+                                    />
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                )}
+                {loading && !noTask && (
+                    <div className="font-mono text-2xl font-bold">
+                        Loading...
+                    </div>
+                )}
+                {noTask && !loading && <NoTask />}
+            </div>
+        </>
     );
-    if (response) {
-      const nextTask = await axios.get(`${BACKEND_URL}/nextTask`, {
-        headers: {
-          authorization: localStorage.getItem("token"),
-        },
-      });
-      if (!nextTask.data?.tasks) {
-        setNoTask(true);
-      } else {
-        let data: Task = {
-          id: nextTask.data.tasks.id,
-          title: nextTask.data.tasks.title,
-          options: nextTask.data.tasks.options,
-        };
-        setTask(data);
-      }
-    }
-    const curBalance = await axios.get(`${BACKEND_URL}/balance`, {
-      headers: {
-        authorization: localStorage.getItem("token"),
-      },
-    });
-    if (curBalance) {
-      setBalance(Number(curBalance.data.balance));
-    }
-    setLoading(false);
-  }
-  return (
-    <>
-      {!loading && !noTask && (
-        <div className="">
-          <div className="font-mono text-lg font-bold p-2">
-            {taskData?.title}
-          </div>
-          <div className="p-1">
-            {taskData?.options.map((option) => (
-              <div className="p-1" key={option.id}>
-                <Image
-                  src={option.image_url}
-                  alt={option.id.toString()}
-                  height={400}
-                  width={400}
-                  onClick={() => {
-                    handleImageClick(option.id);
-                  }}
-                  className="cursor-pointer border-black rounded m-2"
-                />
-              </div>
-            ))}
-          </div>
-        </div>
-      )}
-      {loading && !noTask && (
-        <div className="font-mono text-2xl font-bold">Loading...</div>
-      )}
-      {noTask && !loading && <NoTask />}
-    </>
-  );
 };
 
 export default TakeVote;
